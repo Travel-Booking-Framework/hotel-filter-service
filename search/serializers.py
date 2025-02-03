6cdb54a12bea4db6590e5cefe33ef9ce502108c8
alireza-hme <hme.alireza@gmail.com>
2025-02-04 01:33:25 +0330

    Modify seralizer

--- search/serializers.py
+++ search/serializers.py
@@ -1,13 +1,21 @@
 from rest_framework import serializers
 
 class HotelSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
+    id = serializers.CharField()
     name = serializers.CharField()
     location = serializers.CharField()
-    price = serializers.FloatField()
+    rating = serializers.FloatField()
+    price_per_night = serializers.DecimalField(max_digits=10, decimal_places=2)
+    amenities = serializers.ListField(child=serializers.CharField())
 
-    def validate_price(self, value):
-        """Ensure the price is not negative."""
-        if value < 0:
-            raise serializers.ValidationError("Price must be a positive number.")
-        return value
+    def to_representation(self, instance):
+        """Convert Elasticsearch result into a standardized format."""
+        source = instance["_source"]
+        return {
+            "id": instance["_id"],
+            "name": source.get("name"),
+            "location": source.get("location"),
+            "rating": source.get("rating"),
+            "price_per_night": source.get("price_per_night"),
+            "amenities": source.get("amenities", []),
+        }
